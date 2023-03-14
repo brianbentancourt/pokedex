f8d82b86dcdc8c76ef9a8c9f5eeedc4ca3b4c222
Brian Bentancourt <brianbentancourt9@gmail.com>
2023-03-14 11:45:09 -0300

    list of favorites

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,18 +1,43 @@
 import { SafeAreaView, Text } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
 import { getPokemonsFavoriteApi } from '../api/favorite'
+import { getPokemonDetailsApi } from '../api/pokemon'
+import useAuth from '../hooks/useAuth'
+import PokemonList from '../components/PokemonList'
 
 export default function Favorite() {
-    const [favorites, setFavorites] = useState(null)
+    const [favorites, setFavorites] = useState([])
+    const { auth } = useAuth()
 
-    useEffect(() => {
-        (async () => setFavorites(await getPokemonsFavoriteApi()))()
-    }, [])
+    useFocusEffect(
+        useCallback(() => {
+            setFavorites([])
+            auth && (async () => {
+                const response = await getPokemonsFavoriteApi()
+                const pokemonsArray = []
+                for await (const id of response) {
+                    const pokemonDetails = await getPokemonDetailsApi(id)
+                    pokemonsArray.push({
+                        id: pokemonDetails.id,
+                        name: pokemonDetails.name,
+                        type: pokemonDetails.types[0].type.name,
+                        order: pokemonDetails.order,
+                        image: pokemonDetails.sprites.other['official-artwork'].front_default
+                    })
+                }
+                setFavorites(pokemonsArray)
+            })()
+        }, [auth])
+    )
 
+
+    if (!auth)
+        return (<Text>Usuario no logueado</Text>)
 
     return (
         <SafeAreaView>
-            <Text>Favorite</Text>
+            <PokemonList pokemons={favorites} />
         </SafeAreaView>
     )
 }
